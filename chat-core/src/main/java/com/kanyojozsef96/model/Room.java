77ee6a866c9a5087aff27d2f941b860e80c88551
Kanyó József <kanyojozsef96@gmail.com>
2021-03-26 15:00:01 +0100

    base model classes added

---
+++ chat-core/src/main/java/com/kanyojozsef96/model/Room.java
@@ -0,0 +1,84 @@
+package com.kanyojozsef96.model;
+
+import javafx.beans.property.*;
+import javafx.collections.ObservableList;
+
+public class Room {
+    public enum RoomType {
+        FRIENDS("Friends"),
+        WORK("Work"),
+        SPORT("Sport"),
+        LOVE("Love"),
+        EDUCATION("Education"),
+        HEALTH("Health"),
+        POLITICS("Politics");
+
+        private final StringProperty value = new SimpleStringProperty(this, "value");
+
+        RoomType(String v) {
+            this.setValue(v);
+        }
+
+        public String getValue() {
+            return value.get();
+        }
+
+        public StringProperty valueProperty() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value.set(value);
+        }
+
+        @Override
+        public String toString() {
+            return this.getValue();
+        }
+    }
+
+    private StringProperty name = new SimpleStringProperty(this, "name");
+    private ListProperty<String> rules = new SimpleListProperty<>(this, "rules");
+    private ObjectProperty<RoomType> roomType = new SimpleObjectProperty<>(this, "roomType");
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public ObservableList<String> getRules() {
+        return rules.get();
+    }
+
+    public ListProperty<String> rulesProperty() {
+        return rules;
+    }
+
+    public void setRules(ObservableList<String> rules) {
+        this.rules.set(rules);
+    }
+
+    public RoomType getRoomType() {
+        return roomType.get();
+    }
+
+    public ObjectProperty<RoomType> roomTypeProperty() {
+        return roomType;
+    }
+
+    public void setRoomType(RoomType roomType) {
+        this.roomType.set(roomType);
+    }
+
+    @Override
+    public String toString() {
+        return this.getName() + "--" + this.getRoomType().toString();
+    }
+}
---
+++ chat-core/src/main/java/com/kanyojozsef96/model/User.java
@@ -0,0 +1,90 @@
+package com.kanyojozsef96.model;
+
+import javafx.beans.property.*;
+import javafx.collections.ObservableList;
+
+public class User {
+    private IntegerProperty id = new SimpleIntegerProperty(this, "id");
+    private StringProperty username = new SimpleStringProperty(this, "username");
+    private StringProperty password = new SimpleStringProperty(this, "password");
+    private IntegerProperty age = new SimpleIntegerProperty(this, "age");
+    private StringProperty sex = new SimpleStringProperty(this, "sex");
+    private ListProperty<String> hobbies = new SimpleListProperty<>(this, "hobbies");
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getUsername() {
+        return username.get();
+    }
+
+    public StringProperty usernameProperty() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username.set(username);
+    }
+
+    public String getPassword() {
+        return password.get();
+    }
+
+    public StringProperty passwordProperty() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password.set(password);
+    }
+
+    public int getAge() {
+        return age.get();
+    }
+
+    public IntegerProperty ageProperty() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age.set(age);
+    }
+
+    public String getSex() {
+        return sex.get();
+    }
+
+    public StringProperty sexProperty() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex.set(sex);
+    }
+
+    public ObservableList<String> getHobbies() {
+        return hobbies.get();
+    }
+
+    public ListProperty<String> hobbiesProperty() {
+        return hobbies;
+    }
+
+    public void setHobbies(ObservableList<String> hobbies) {
+        this.hobbies.set(hobbies);
+    }
+
+    @Override
+    public String toString() {
+        return this.getUsername();
+    }
+}
