8bde4e641e7e6edcb930aecb8bade1bdba7a6bfa
Kanyó József <kanyojozsef96@gmail.com>
2021-03-30 18:55:31 +0200

    added application.properties util class

---
+++ chat-core/src/main/java/com/kanyojozsef96/config/PropertiesUtil.java
@@ -0,0 +1,22 @@
+package com.kanyojozsef96.config;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesUtil {
+    private static final Properties props = new Properties();
+
+    static {
+        try {
+            props.load(PropertiesUtil.class.getResourceAsStream("/application.properties"));
+        } catch (IOException ex) {
+            System.out.println("Couldn't find the properties file to load, printing stacktrace...");
+            ex.printStackTrace();
+        }
+    }
+
+    private PropertiesUtil() {}
+
+    public static Properties getProps() { return props; }
+    public static String getUtilPropValue(String key) { return props.getProperty(key); }
+}
