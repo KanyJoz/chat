af7daefa89e1f183ff06f29352316d53b87fe828
Kanyó József <kanyojozsef96@gmail.com>
2021-03-30 18:44:58 +0200

    refactor existing code

--- chat-core/src/main/java/com/kanyojozsef96/model/Room.java
+++ chat-core/src/main/java/com/kanyojozsef96/model/Room.java
@@ -19,93 +19,44 @@ public class Room {
             this.setValue(v);
         }
 
-        public String getValue() {
-            return value.get();
-        }
-
-        public StringProperty valueProperty() {
-            return value;
-        }
-
-        public void setValue(String value) {
-            this.value.set(value);
-        }
+        public String getValue() { return value.get(); }
+        public StringProperty valueProperty() { return value; }
+        public void setValue(String value) { this.value.set(value); }
 
         @Override
         public String toString() {
-            return this.getValue();
+            return "Room Type --> " + this.getValue();
         }
     }
 
     private IntegerProperty id = new SimpleIntegerProperty(this, "id");
     private StringProperty name = new SimpleStringProperty(this, "name");
     private ObjectProperty<RoomType> roomType = new SimpleObjectProperty<>(this, "roomType");
-
     private ListProperty<String> rules = new SimpleListProperty<>(this, "rules");
     private ListProperty<User> users = new SimpleListProperty<>(this, "users");
 
-    public int getId() {
-        return id.get();
-    }
+    public int getId() { return id.get(); }
+    public IntegerProperty idProperty() { return id; }
+    public void setId(int id) { this.id.set(id); }
 
-    public IntegerProperty idProperty() {
-        return id;
-    }
+    public String getName() { return name.get(); }
+    public StringProperty nameProperty() { return name; }
+    public void setName(String name) { this.name.set(name); }
 
-    public void setId(int id) {
-        this.id.set(id);
-    }
+    public ObservableList<String> getRules() { return rules.get(); }
+    public ListProperty<String> rulesProperty() { return rules; }
+    public void setRules(ObservableList<String> rules) { this.rules.set(rules); }
 
-    public String getName() {
-        return name.get();
-    }
+    public RoomType getRoomType() { return roomType.get(); }
+    public ObjectProperty<RoomType> roomTypeProperty() { return roomType; }
+    public void setRoomType(RoomType roomType) { this.roomType.set(roomType); }
 
-    public StringProperty nameProperty() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name.set(name);
-    }
-
-    public ObservableList<String> getRules() {
-        return rules.get();
-    }
-
-    public ListProperty<String> rulesProperty() {
-        return rules;
-    }
-
-    public void setRules(ObservableList<String> rules) {
-        this.rules.set(rules);
-    }
-
-    public RoomType getRoomType() {
-        return roomType.get();
-    }
-
-    public ObjectProperty<RoomType> roomTypeProperty() {
-        return roomType;
-    }
-
-    public void setRoomType(RoomType roomType) {
-        this.roomType.set(roomType);
-    }
-
-    public ObservableList<User> getUsers() {
-        return users.get();
-    }
-
-    public ListProperty<User> usersProperty() {
-        return users;
-    }
-
-    public void setUsers(ObservableList<User> users) {
-        this.users.set(users);
-    }
+    public ObservableList<User> getUsers() { return users.get(); }
+    public ListProperty<User> usersProperty() { return users; }
+    public void setUsers(ObservableList<User> users) { this.users.set(users); }
 
     @Override
     public String toString() {
-        return this.getName() + "--" + this.getRoomType().toString();
+        return "Room Name --> " + this.getName() + " -- " + this.getRoomType().toString();
     }
 }
--- chat-core/src/main/java/com/kanyojozsef96/model/User.java
+++ chat-core/src/main/java/com/kanyojozsef96/model/User.java
@@ -12,92 +12,36 @@ public class User {
     private StringProperty sex = new SimpleStringProperty(this, "sex");
     private ListProperty<String> hobbies = new SimpleListProperty<>(this, "hobbies");
 
-    public int getId() {
-        return id.get();
-    }
+    public int getId() { return id.get(); }
+    public IntegerProperty idProperty() { return id; }
+    public void setId(int id) { this.id.set(id); }
 
-    public IntegerProperty idProperty() {
-        return id;
-    }
+    public String getUsername() { return username.get(); }
+    public StringProperty usernameProperty() { return username; }
+    public void setUsername(String username) { this.username.set(username); }
 
-    public void setId(int id) {
-        this.id.set(id);
-    }
+    public String getPassword() { return password.get(); }
+    public StringProperty passwordProperty() { return password; }
+    public void setPassword(String password) { this.password.set(password); }
 
-    public String getUsername() {
-        return username.get();
-    }
+    public String getEmail() { return email.get(); }
+    public StringProperty emailProperty() { return email; }
+    public void setEmail(String email) { this.email.set(email); }
 
-    public StringProperty usernameProperty() {
-        return username;
-    }
+    public int getAge() { return age.get(); }
+    public IntegerProperty ageProperty() { return age; }
+    public void setAge(int age) { this.age.set(age); }
 
-    public void setUsername(String username) {
-        this.username.set(username);
-    }
+    public String getSex() { return sex.get(); }
+    public StringProperty sexProperty() { return sex; }
+    public void setSex(String sex) { this.sex.set(sex); }
 
-    public String getPassword() {
-        return password.get();
-    }
-
-    public StringProperty passwordProperty() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password.set(password);
-    }
-
-    public String getEmail() {
-        return email.get();
-    }
-
-    public StringProperty emailProperty() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email.set(email);
-    }
-
-    public int getAge() {
-        return age.get();
-    }
-
-    public IntegerProperty ageProperty() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age.set(age);
-    }
-
-    public String getSex() {
-        return sex.get();
-    }
-
-    public StringProperty sexProperty() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex.set(sex);
-    }
-
-    public ObservableList<String> getHobbies() {
-        return hobbies.get();
-    }
-
-    public ListProperty<String> hobbiesProperty() {
-        return hobbies;
-    }
-
-    public void setHobbies(ObservableList<String> hobbies) {
-        this.hobbies.set(hobbies);
-    }
+    public ObservableList<String> getHobbies() { return hobbies.get(); }
+    public ListProperty<String> hobbiesProperty() { return hobbies; }
+    public void setHobbies(ObservableList<String> hobbies) { this.hobbies.set(hobbies); }
 
     @Override
     public String toString() {
-        return this.getUsername();
+        return "Username --> " + this.getUsername();
     }
 }
