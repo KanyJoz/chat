3a0f6d88b2c1e60cd28b76a4e2d5a5b351da98a4
Kanyó József <kanyojozsef96@gmail.com>
2021-03-30 21:31:17 +0200

    refactor layout files

--- chat-desktop/src/main/java/com/kanyojozsef96/controller/MainWindowController.java
+++ chat-desktop/src/main/java/com/kanyojozsef96/controller/MainWindowController.java
@@ -1,4 +1,18 @@
 package com.kanyojozsef96.controller;
 
+import com.kanyojozsef96.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class MainWindowController {
+
+    @FXML
+    public void onListUsersBtnClicked(ActionEvent actionEvent) {
+        App.loadFXML("/fxml/window_users.fxml", null);
+    }
+
+    @FXML
+    public void onListRoomsBtnClicked(ActionEvent actionEvent) {
+        App.loadFXML("/fxml/window_rooms.fxml", null);
+    }
 }
--- chat-desktop/src/main/java/com/kanyojozsef96/controller/RoomsWindowController.java
+++ chat-desktop/src/main/java/com/kanyojozsef96/controller/RoomsWindowController.java
@@ -1,4 +1,19 @@
 package com.kanyojozsef96.controller;
 
+import com.kanyojozsef96.App;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class RoomsWindowController {
+
+    @FXML
+    public void onBackMenuItemClicked(ActionEvent actionEvent) {
+        App.loadFXML("/fxml/window_main.fxml", null);
+    }
+
+    @FXML
+    public void onCloseMenuItemClicked(ActionEvent actionEvent) {
+        Platform.exit();
+    }
 }
