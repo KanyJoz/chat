ee225a12b6805f7bcf9d34f6950d6485f74d9bb7
Kanyó József <kanyojozsef96@gmail.com>
2021-03-30 19:09:29 +0200

    main fxmlloader refactor

--- chat-desktop/src/main/java/com/kanyojozsef96/App.java
+++ chat-desktop/src/main/java/com/kanyojozsef96/App.java
@@ -7,25 +7,41 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 
 /**
  * JavaFX App
  */
 public class App extends Application {
+    private static Stage mainStage;
 
     @Override
     public void start(Stage stage) {
+        App.mainStage = stage;
+        loadFXML("/fxml/window_main.fxml", null);
+        stage.show();
+    }
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/window_main.fxml"));
+    public static FXMLLoader loadFXML(String fxmlURL, Stage optionalWindow) {
+        FXMLLoader loader = new FXMLLoader(App.class.getResource(fxmlURL));
+        Scene scene = null;
+
         try {
-            Parent root = fxmlLoader.load();
-            stage.setScene(new Scene(root));
+            Parent root = loader.load();
+            scene = new Scene(root);
         } catch (IOException e) {
+            System.out.println("Cannot load fxml file, printing stacktrace...");
             e.printStackTrace();
         }
 
-        stage.show();
+        if(Objects.nonNull(optionalWindow)) {
+            optionalWindow.setScene(scene);
+        } else {
+            App.mainStage.setScene(scene);
+        }
+
+        return loader;
     }
 
     public static void main(String[] args) {
